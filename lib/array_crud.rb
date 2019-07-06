1e64fa0d0f30f4c39ce55cf9ae84cb782eea17fa
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 23:43:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ p  create_an_array<< "arrays!"
  end
 
 def add_element_to_start_of_array(array, element)
- create_an_array.unshift= "wow"
+ create_an_array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
