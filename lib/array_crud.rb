4d30a88e77541629b33056b03db93e2c1e3f5c55
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 23:26:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["dad", "mom","sister","brother"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
-end
+def add_element_to_end_of_array(array,element)
+p  create_an_empty_array<< "arrays!"
+ end
 
 def add_element_to_start_of_array(array, element)
   
