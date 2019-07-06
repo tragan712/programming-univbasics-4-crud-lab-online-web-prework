df1535ef8d367cf5ed76672afde2509469a8ece0
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 23:59:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array,element)
-p  << "arrays!"
+p add_element_to_end_of_array << "arrays!"
  end
 
 def add_element_to_start_of_array(array, element)
-p create_an_array.unshift "wow"
+p .unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
