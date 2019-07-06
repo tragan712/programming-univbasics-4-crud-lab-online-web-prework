cce0916e69fa2d075e4b2c94e3dcfd7c0242048e
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 23:59:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,25 +15,25 @@ p create_an_array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
- p create_an_array.pop "arrays!"
+ p .pop "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-p wow create_an_array.shift 
+p .shift 
 end
 
 def retrieve_element_from_index(array, index_number)
- p create_an_array[2]
+ p[2]
 end
 
 def retrieve_first_element_from_array(array)
-  p create_an_array[0]
+  p [0]
 end
 
 def retrieve_last_element_from_array(array)
-p  create_an_array[-1]
+p  [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-p create_an_array[4]="totally"
+p [4]="totally"
 end
