87c0e1eea2f02fb302dd6ec0c1a5f112f17996d3
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 23:29:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array,element)
-p  create_an_empty_array<< "arrays!"
+p  create_anpty_arr_emay<< "arrays!"
  end
 
 def add_element_to_start_of_array(array, element)
