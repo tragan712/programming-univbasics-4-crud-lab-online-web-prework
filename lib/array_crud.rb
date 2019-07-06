5a773ac818a6fbac667f43d4340e1a35bb87524f
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 23:45:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
- p create_an_array. "arrays!"
+ p create_an_array.pop "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
