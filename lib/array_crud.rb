e4361d146054469c483321ca03445aa2b0ddf255
Timothy Ragan <timothyragan7@gmail.com>
2019-07-06 23:46:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ p  create_an_array<< "arrays!"
  end
 
 def add_element_to_start_of_array(array, element)
- create_an_array.unshift "wow"
+p create_an_array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift "wow"
+p  create_an_array.shift "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
