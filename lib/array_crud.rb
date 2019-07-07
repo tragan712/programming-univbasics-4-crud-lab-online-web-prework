c0a5e7dfa730733a89b78afbbbb8a89e070aa79f
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 00:08:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-remove_element_from_start_of_array.shift "wow"
+p  remove_element_from_start_of_array.shift "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-p  retrieve_element_from_index[2]
+puts  retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
