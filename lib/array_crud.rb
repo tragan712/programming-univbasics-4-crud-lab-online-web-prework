079d372d4c7a40716f051730b99381f4315f536e
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 00:26:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array,element)
  end
 
 def add_element_to_start_of_array(array, element)
- array.unshift"wow"
+ array.unshift,"wow"
 end
 
 def remove_element_from_end_of_array(array)
@@ -31,7 +31,7 @@ p  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
- p retrieve_last_element_from_array=
+ p retrieve_last_element_from_array=[-1]
 end
 
 def update_element_from_index(array, index_number, element)
