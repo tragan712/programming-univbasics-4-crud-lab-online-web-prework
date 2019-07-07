59686a80887e04d48ec652f0e1171a86d7a44b3f
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 00:26:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-p  retrieve_first_element_from_array[0]
+p  retrieve_first_element_from_array=[0]
 end
 
 def retrieve_last_element_from_array(array)
