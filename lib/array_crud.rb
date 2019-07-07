cbc94f410b97b48c1748f1166eb85024366b17f6
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 00:01:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,33 +7,33 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array,element)
-p add_element_to_end_of_array << "arrays!"
+ add_element_to_end_of_array << "arrays!"
  end
 
 def add_element_to_start_of_array(array, element)
-p add_element_to_end_of_array.unshift "wow"
+ add_element_to_end_of_array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
- p add_element_to_end_of_array.pop "arrays!"
+  add_element_to_end_of_array.pop "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-p add_element_to_end_of_array.shift 
+ add_element_to_end_of_array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
- p add_element_to_end_of_array[2]
+  add_element_to_end_of_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  p add_element_to_end_of_array[0]
+  add_element_to_end_of_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-p  add_element_to_end_of_array[-1]
+  add_element_to_end_of_array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-p add_element_to_end_of_array[4]="totally"
+ add_element_to_end_of_array[4]="totally"
 end
