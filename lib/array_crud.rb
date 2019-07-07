e4ea3dd0c3a9548790522b4b5c3b9117c1b9401d
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 01:09:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ p "arrays!" .pop array
 end
 
 def remove_element_from_start_of_array(array)
- p  .shift Array "wow"
+ "wow"= Array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
