3efcff7321609a0fc54c314eac5d5897e4fd68be
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 01:23:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,29 @@
 def create_an_empty_array
- p []
+  []
 end
 def create_an_array
-p  ["dad", "mom","sister","brother"]
+  ["dad", "mom","sister","brother"]
 end
 def add_element_to_end_of_array(array,element)
- p  array << "arrays!"
+   array << "arrays!"
 end
 def add_element_to_start_of_array(array, element)
-p   array.unshift "wow"
+   array.unshift "wow"
 end
 def remove_element_from_end_of_array(array)
-p  array.pop["arrays!"]
+  array.pop["arrays!"]
 end
 def remove_element_from_start_of_array(array)
- p array.shift["wow"]
+  array.shift["wow"]
 end
 def retrieve_element_from_index(array, index_number)
-p  array[2]
+ array[2]
 end
 def retrieve_first_element_from_array(array)
-p  array[0]
+  array[0]
 end
 def retrieve_last_element_from_array(array)
-p array[-1]
+ array[-1]
 end
 def update_element_from_index(array, index_number, element)
    array[4]="totally"
