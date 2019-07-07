fba6ae41080798406fc075021024cd24f90e6b55
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 01:23:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def retrieve_first_element_from_array(array)
   array[0]
 end
 def retrieve_last_element_from_array(array)
- array[-1]
+  array[-1]
 end
 def update_element_from_index(array, index_number, element)
    array[4]="totally"
