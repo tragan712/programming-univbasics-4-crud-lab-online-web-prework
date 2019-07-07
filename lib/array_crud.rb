168dffe11d7cea5cb25a6feee00ef4cec996ad67
Timothy Ragan <timothyragan7@gmail.com>
2019-07-07 00:00:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-p .shift 
+p add_element_to_end_of_array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
